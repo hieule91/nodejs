1ffadcfb299692b425a2fdcca2c574373083d275
jackle91 <hieu6@gatech.edu>
2017-12-09 18:53:31 -0500

    playing with lodash

--- myApp.js
+++ myApp.js
@@ -1,18 +1,27 @@
 console.log('Starting myApp.js');
 
-const fs = require('fs');
+// nodemon works just like "grunt serve"
 
+const fs = require('fs'); // in nodejs lib
 const os = require('os');
 
-const notes = require('./myNotes.js');
+const _ = require('lodash'); // third party
 
-var res = notes.addNote();
-console.log(res);
+const notes = require('./myNotes.js'); // new sub file in the same dir
+
+// var res = notes.addNote();
+// console.log(res);
 
 // var sum = notes.add(5, 6);
-console.log('a + b =', notes.add(5, 6));
+// console.log('a + b =', notes.add(5, 6));
 
 // var user = os.userInfo();
 // console.log(user);
+
+// console.log(_.isString(true));
+// console.log(_.isString('true'));
+
+var filteredArray = _.uniq(['Jack', 1, true, 1, 'Jack', true, true, 1 , 2, 'haha']); // [] for arr declaration
+console.log(filteredArray);
 
 // fs.appendFileSync('hello.txt', `Hello, ${user.username}!`);
