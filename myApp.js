1174ee921bda3e64dd237dee700549d22988515f
jackle91 <hieu6@gatech.edu>
2017-12-11 14:39:02 -0500

    non-refactor

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 console.log('Starting myApp.js');
 
+// run "nodemon myApp.js"
 // nodemon works just like "grunt serve"
 
 const fs = require('fs'); // in nodejs lib
@@ -7,7 +8,11 @@ const os = require('os');
 
 const _ = require('lodash'); // third party
 
+const yargs = require('yargs'); // third party
+
 const notes = require('./myNotes.js'); // new sub file in the same dir
+
+const argv = yargs.argv;
 
 // var res = notes.addNote();
 // console.log(res);
@@ -21,7 +26,48 @@ const notes = require('./myNotes.js'); // new sub file in the same dir
 // console.log(_.isString(true));
 // console.log(_.isString('true'));
 
-var filteredArray = _.uniq(['Jack', 1, true, 1, 'Jack', true, true, 1 , 2, 'haha']); // [] for arr declaration
-console.log(filteredArray);
+// var filteredArray = _.uniq(['Jack', 1, true, 1, 'Jack', true, true, 1 , 2, 'haha']); // [] for arr declaration
+// console.log(filteredArray);
+
+// in Terminal: press "node myApp.js argv[] --title "secrets 2""
+// 				   vs "node myApp.js argv[] --title="secrets 2""
+
+// var command = process.argv[2];
+var command = argv._[0]; // same as the above
+
+console.log('Command:', command);
+
+// console.log('Process:', process.argv);
+
+console.log('Yargs:', argv);
+
+if (command === 'add') {
+	// console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+	// console.log('Listing all notes');
+	notes.getAll();	
+} else if (command === 'read') {
+	// console.log('Reading a note');
+	notes.read(argv.title);
+} else if (command === 'remove') {
+	// console.log('Removing a note'); 
+	notes.remove(argv.title);
+} else {
+	console.log('Command not found');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // fs.appendFileSync('hello.txt', `Hello, ${user.username}!`);
--- myNotes.js
+++ myNotes.js
@@ -1,11 +1,90 @@
 console.log('Starting myNotes.js');
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New Note';
+const fs = require('fs');
+
+var fetchNotes = () => {
+	
 };
 
-module.exports.add = (a, b) => {
-	return a + b;
+var saveNotes = (notes) => {
+
 };
+
+var addNote = (title, body) => {
+	// console.log('Adding note', title, body);
+	var notes = [];
+	var note = {
+		title,
+		body
+	};
+
+	// load file
+	try {
+		var noteString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(noteString);	
+	} catch (e) {
+
+	}
+
+	// var duplicateNotes = notes.filter((note) => {
+	// 	return duplicateNotes.title = title;
+	// });
+
+	var duplicateNotes = notes.filter((note) => note.title === title);
+	// arrow function
+
+	// write to file
+	if (duplicateNotes.length === 0) {
+		notes.push(note); // add a single note into notes arr
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+		// convert notes obj to string and write into notes-data	
+	}
+	
+};
+
+var getAll = () => {
+	console.log('Getting all notes');
+};
+
+var read = (title) => {
+	console.log('Reading notes', title);
+};
+
+var remove = (title) => {
+	console.log('Remove a note', title);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	read,
+	remove
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports.addNote = () => {
+// 	console.log('addNote');
+// 	return 'New Note';
+// };
+
+// module.exports.add = (a, b) => {
+// 	return a + b;
+// };
 
